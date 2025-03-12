e0872d4dbbf7a0da88617d6dc22c6f33005785f6
MahammadSultanov <mehemmedsultanli@gmail.com>
2025-03-12 12:50:41 +0400

    Basic image changer

--- proudemy/ViewController.swift
+++ proudemy/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonChanger(_ sender: Any) {
+        imageView.image = UIImage(named: "lp2")
+        
+    }
+    
 }
 
