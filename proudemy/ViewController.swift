dbf9ce32742f79c06a46ba3427bbbe78c0720809
MahammadSultanov <mehemmedsultanli@gmail.com>
2025-03-12 12:33:55 +0400

    Basic image changer

---
+++ proudemy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proudemy
+//
+//  Created by Mahammad on 12.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
